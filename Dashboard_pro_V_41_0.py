a839a887da8dade099f7001fa2212ac5bda18a79
crimifrancesco-art <crimi.francesco@gmail.com>
2026-04-15 09:03:52 +0200

    Update Dashboard_pro_V_41_0.py

--- Dashboard_pro_V_41_0.py
+++ Dashboard_pro_V_41_0.py
@@ -2402,7 +2402,7 @@ if st.sidebar.button("↺ Reset layout griglie",key="reset_grid_layout",use_cont
 # SCANNER
 # =========================================================================
 if not only_watchlist:
-    if st.button("🚀 AVVIA SCANNER PRO 39.0",type="primary",use_container_width=True):
+    if st.button("🚀 AVVIA SCANNER PRO 41.0",type="primary",use_container_width=True):
         universe = load_universe(sel)
         if not universe:
             st.warning("Seleziona almeno un mercato!")
